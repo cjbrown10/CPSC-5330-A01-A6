3da16c263fd2092ff549f739ad4f1a305978a52d
Colby Brown <cjb0154@auburn.edu>
2026-02-11 17:31:16 -0600

    Initial Commit

---
+++ Assignment6_CurrencyConverter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assignment6_CurrencyConverter
+//
+//  Created by Colby Brown on 2/11/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
